899912d8705698b4abe01ae4c81fca2429b3db23
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-11-18 06:38:32 +0100

    proj setup

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,21 @@
+interface repositoriesState {
+    loading: boolean,
+    data: string[],
+    error: string | null
+}
+
+const reducer = (state:repositoriesState ,action:any) => {
+    switch(action.type){
+        case 'search_repositories':
+            return {loading: true, error: null, data: []}
+        case 'search_repositories_success':
+            return {loading: false, error: null, data: action.payload}
+        case 'search_repositories_error':
+            return {loading: false, error: action.payload, data: []}
+        default:
+            return
+    }
+
+}
+
+export default reducer
