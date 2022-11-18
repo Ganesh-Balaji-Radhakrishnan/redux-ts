9d1e64174e22adf2a67ebe10ddb4ff8f9e5c25b3
Ganesh Balaji Radhakrishnan <ganesh.balaji@brighter.se>
2022-11-18 21:21:30 +0100

    seting up redux store

---
+++ src/state/action-dispatch/index.ts
@@ -0,0 +1,20 @@
+import { ActionType } from "../action-types/action-types";
+import { Action } from "../actions";
+
+const searchRepositories = (term:string) => {
+    return async (dispatch:any) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES
+        })
+
+        try {}
+        catch (err){
+            if (err instanceof Error) {
+                dispatch({
+                  type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                  payload: err.message,
+                });
+        }
+    }
+    }
+}
--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,21 +1,26 @@
-interface repositoriesState {
-    loading: boolean,
-    data: string[],
-    error: string | null
-}
-
-const reducer = (state:repositoriesState ,action:any) => {
-    switch(action.type){
-        case 'search_repositories':
-            return {loading: true, error: null, data: []}
-        case 'search_repositories_success':
-            return {loading: false, error: null, data: action.payload}
-        case 'search_repositories_error':
-            return {loading: false, error: action.payload, data: []}
-        default:
-            return
+import { ActionType } from "../action-types/action-types";
+import { Action } from "../actions";
+interface RepositoriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[];
+  }
+  
+const reducer = (
+    state: RepositoriesState,
+    action: Action
+  ): RepositoriesState => {
+    switch (action.type) {
+      case ActionType.SEARCH_REPOSITORIES:
+        return { loading: true, error: null, data: [] };
+      case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+        return { loading: false, error: null, data: action.payload };
+      case ActionType.SEARCH_REPOSITORIES_ERROR:
+        return { loading: false, error: action.payload, data: [] };
+      default:
+        return state;
     }
-
-}
-
-export default reducer
+  };
+  
+  export default reducer;
+  
